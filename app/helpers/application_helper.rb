443bb87a33b1603c8421bd3ec78c823958d28323
Eugene Osadchiy <silver.arrow.odessa@gmail.com>
2015-12-11 01:03:23 +0200

    Redefine default_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def domain(path)
-    unless Settings.port.blank?
-      "#{Settings.protocol}://#{Settings.domain}:#{Settings.port}#{path}"
-    else
-      "#{Settings.protocol}://#{Settings.domain}#{path}"
-    end
-  end
 end
