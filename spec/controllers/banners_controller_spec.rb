081d2a58b1205611633332b2ce51038f9c08801c
Eugene Osadchiy <silver.arrow.odessa@gmail.com>
2015-12-10 18:13:03 +0200

    Add banner controller spec, but with comments

--- spec/controllers/banners_controller_spec.rb
+++ spec/controllers/banners_controller_spec.rb
@@ -2,11 +2,34 @@ require 'rails_helper'
 
 RSpec.describe BannersController, type: :controller do
 
+  describe "GET #index" do
+    it "returns http success" # do
+#       get :index, use_route: :advertising_platforms
+#       expect(response).to have_http_status(:success)
+#     end
+    it "renders the 'show' template of advertising platforms controller" # do
+#       get :index, use_route: :advertising_platforms
+#       expect(response).to render_template('advertising_platforms/show')
+#     end
+  end
+
   describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
+    it "returns http success" # do
+#       get :new, use_route: :advertising_platforms
+#       expect(response).to have_http_status(:success)
+#     end
+    it "renders the 'new' template" # do
+#       get :new, use_route: :advertising_platforms
+#       expect(response).to render_template(:new)
+#     end
+  end
+
+  describe "GET #edit" do
+    it "returns http success"
+  end
+
+  describe "GET #show" do
+    it "returns http success"
   end
 
 end
