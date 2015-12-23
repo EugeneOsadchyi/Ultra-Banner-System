35a73b3fe6f0deee6d9541280c8377fa1db52e63
Eugene Osadchiy <silver.arrow.odessa@gmail.com>
2015-12-24 01:10:13 +0200

    Fix flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def bootstrap_class_for flash_type
-    { success: 'alert-success', error: 'alert-danger', warning: 'alert-warning'}[flash_type.to_sym]
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym]
   end
 
   def flash_messages(opts = {})
